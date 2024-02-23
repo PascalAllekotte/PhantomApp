66a2cba726ee80d6988c735c7867227f1fb62dbf
pascalallekotte <pascalallekotte1@web.de>
2024-02-23 22:16:09 +0100

    mvvm

--- app/src/main/java/de/syntax/androidabschluss/ui/HomeFragment.kt
+++ app/src/main/java/de/syntax/androidabschluss/ui/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -28,6 +29,8 @@ private lateinit var binding: FragmentHomeBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
 
         (activity as? MainActivity)?.findViewById<BottomNavigationView>(R.id.bottomNavigationView)?.visibility =
             View.GONE
