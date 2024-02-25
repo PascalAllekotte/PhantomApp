3127f0c2c022f7ea793e0cf9179734bb27847474
pascalallekotte <pascalallekotte1@web.de>
2024-02-25 19:26:47 +0100

    settingsui einrichten

---
+++ app/src/main/java/de/syntax/androidabschluss/ui/SettingsFragment.kt
@@ -0,0 +1,37 @@
+package de.syntax.androidabschluss.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import de.syntax.androidabschluss.R
+import de.syntax.androidabschluss.databinding.FragmentLoginBinding
+import de.syntax.androidabschluss.databinding.FragmentSettingsBinding
+import de.syntax.androidabschluss.viewmodel.FirebaseViewModel
+
+
+class SettingsFragment : Fragment() {
+
+    private lateinit var binding: FragmentSettingsBinding
+    private val viewmodel : FirebaseViewModel by activityViewModels()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+
+
+}
