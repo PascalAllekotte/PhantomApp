4b3bbcd5abcf8c9f8a75e336b67dfd85ca8a49ab
pascalallekotte <pascalallekotte1@web.de>
2024-03-05 17:28:03 +0100

    vokabeln xml

--- app/src/main/java/de/syntax/androidabschluss/adapter/local/VokabelDataBaseDao.kt
+++ app/src/main/java/de/syntax/androidabschluss/adapter/local/VokabelDataBaseDao.kt
@@ -3,15 +3,19 @@ package de.syntax.androidabschluss.adapter.local
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Update
 import de.syntax.androidabschluss.data.model.open.VocabItem
 
 
 //tes
 @Dao
-interface {
+interface VokabelDataBaseDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(vokabeln: VocabItem)
+
+    @Update
+    suspend fun update(vokabeln: VocabItem)
 
 
 }
