d4302ecd76e5e97a9d50fa33e847387f533d52a0
pascalallekotte <pascalallekotte1@web.de>
2024-03-12 12:14:03 +0100

    learning detail fragment backbutton

--- app/src/main/java/de/syntax/androidabschluss/ui/LearningDetailFragment.kt
+++ app/src/main/java/de/syntax/androidabschluss/ui/LearningDetailFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import de.syntax.androidabschluss.R
+import de.syntax.androidabschluss.databinding.FragmentLearningDetailBinding
 
 class LearningDetailFragment : Fragment() {
 
+    private lateinit var binding: FragmentLearningDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +22,17 @@ class LearningDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_learning_detail, container, false)
+        binding = FragmentLearningDetailBinding.inflate(inflater,container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.backbutton.setOnClickListener{
+            findNavController().popBackStack()
+        }
     }
 
 }
