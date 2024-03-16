009c8186365af40e20461925fb07e1fdb3189325
pascalallekotte <pascalallekotte1@web.de>
2024-03-17 00:51:57 +0100

    STRUKTURAUFBAU

--- app/src/main/java/de/syntax/androidabschluss/ui/PictureGeneratorFragment.kt
+++ app/src/main/java/de/syntax/androidabschluss/ui/PictureGeneratorFragment.kt
@@ -5,17 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
+
 import androidx.navigation.fragment.findNavController
+import com.squareup.picasso.Picasso
 import de.syntax.androidabschluss.databinding.FragmentPictureGeneratorBinding
+import de.syntax.androidabschluss.viewmodel.ImageGenerationViewModel
+
+
 
 class PictureGeneratorFragment : Fragment() {
     private lateinit var binding: FragmentPictureGeneratorBinding
+    private val imageGenerationViewModel: ImageGenerationViewModel by viewModels()
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
 
     }
@@ -24,7 +31,7 @@ class PictureGeneratorFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentPictureGeneratorBinding.inflate(inflater,container, false)
+        binding = FragmentPictureGeneratorBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -34,13 +41,41 @@ class PictureGeneratorFragment : Fragment() {
 
 
 
-        binding.backbutton.setOnClickListener{
+        binding.backbutton.setOnClickListener {
             findNavController().popBackStack()
 
 
         }
 
+        imageGenerationViewModel.imageUrl.observe(viewLifecycleOwner) { imgUrl ->
+            if (imgUrl != null) {
+                loadImage(imgUrl)
+            } else {
+                // Handle error
+            }
+        }
+
+        imageGenerationViewModel.isInProgress.observe(viewLifecycleOwner) { isInProgress ->
+            setInProgress(isInProgress)
+        }
+
+        binding.btGenerate.setOnClickListener {
+            val text = binding.etInput.text.toString()
+            if (text.isEmpty()) {
+                binding.etInput.setError("No Empty Text")
+            } else {
+                imageGenerationViewModel.generateImage(text)
+            }
+        }
     }
 
+    private fun loadImage(imgUrl: String) {
+        Picasso.get().load(imgUrl).into(binding.imageView)
+        binding.imageView.visibility = View.VISIBLE
+    }
 
+    private fun setInProgress(inProgress: Boolean) {
+        binding.progressbar.visibility = if (inProgress) View.VISIBLE else View.GONE
+        binding.btGenerate.visibility = if (inProgress) View.GONE else View.VISIBLE
+    }
 }
--- app/src/main/java/de/syntax/androidabschluss/ui/SettingsFragment.kt
+++ app/src/main/java/de/syntax/androidabschluss/ui/SettingsFragment.kt
@@ -7,20 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.squareup.picasso.Picasso
 import de.syntax.androidabschluss.R
 import de.syntax.androidabschluss.databinding.FragmentSettingsBinding
 import de.syntax.androidabschluss.viewmodel.FirebaseViewModel
-import de.syntax.androidabschluss.viewmodel.ImageGenerationViewModel
 
 
 class SettingsFragment : Fragment() {
 
     private lateinit var binding: FragmentSettingsBinding
-    private val viewmodel : FirebaseViewModel by activityViewModels()
-    private val imageGenerationViewModel: ImageGenerationViewModel by viewModels()
+    private val viewmodel: FirebaseViewModel by activityViewModels()
 
 
     override fun onCreateView(
@@ -36,43 +32,12 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        imageGenerationViewModel.imageUrl.observe(viewLifecycleOwner) { imgUrl ->
-            if (imgUrl != null) {
-                loadImage(imgUrl)
-            } else {
-                // Handle error
-            }
-        }
-
-        imageGenerationViewModel.isInProgress.observe(viewLifecycleOwner) { isInProgress ->
-            setInProgress(isInProgress)
-        }
-
-
-        binding.logout.setOnClickListener{
+        binding.logout.setOnClickListener {
             viewmodel.logout()
             findNavController().navigate(R.id.homeFragment)
         }
 
 
-        binding.btGenerate.setOnClickListener {
-            val text = binding.etInput.text.toString()
-            if (text.isEmpty()) {
-                binding.etInput.setError("No Empty Text")
-            } else {
-                imageGenerationViewModel.generateImage(text)
-            }
-        }
-    }
-
-    private fun loadImage(imgUrl: String) {
-        Picasso.get().load(imgUrl).into(binding.imageView)
-        binding.imageView.visibility = View.VISIBLE
-    }
-
-    private fun setInProgress(inProgress: Boolean) {
-        binding.progressbar.visibility = if (inProgress) View.VISIBLE else View.GONE
-        binding.btGenerate.visibility = if (inProgress) View.GONE else View.VISIBLE
     }
 }
 
