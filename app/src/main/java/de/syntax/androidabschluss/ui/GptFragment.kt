5751a6e0d5b99200848eb9c69f03f28924fd87a2
pascalallekotte <pascalallekotte1@web.de>
2024-03-17 16:47:38 +0100

    gpt ui

---
+++ app/src/main/java/de/syntax/androidabschluss/ui/GptFragment.kt
@@ -0,0 +1,29 @@
+package de.syntax.androidabschluss.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import de.syntax.androidabschluss.databinding.FragmentGptBinding
+
+
+class GptFragment : Fragment() {
+   private lateinit var binding: FragmentGptBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentGptBinding.inflate(inflater,container,false)
+        return binding.root
+
+    }
+
+
+}
