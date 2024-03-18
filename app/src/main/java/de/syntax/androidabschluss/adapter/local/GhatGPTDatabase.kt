f8c59c90e3ed8cf4b33ed802926a9f019c261dca
pascalallekotte <pascalallekotte1@web.de>
2024-03-18 15:06:59 +0100

    room aufbauen für gpt

--- app/src/main/java/de/syntax/androidabschluss/adapter/local/GhatGPTDatabase.kt
+++ app/src/main/java/de/syntax/androidabschluss/adapter/local/GhatGPTDatabase.kt
@@ -1,7 +1,11 @@
 package de.syntax.androidabschluss.adapter.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import de.syntax.androidabschluss.data.model.open.Chat
 
 
@@ -9,6 +13,30 @@ import de.syntax.androidabschluss.data.model.open.Chat
     entities = [Chat::class],
     version = 1,
     exportSchema = false
-)@TypeConverter(TypeConverter::class)
-class GhatGPTDatabase {
+)
+
+@TypeConverters(TypeConverter::class)
+abstract class GhatGPTDatabase : RoomDatabase() {
+    abstract val chatGptDao : ChatGPTDao
+
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE : GhatGPTDatabase? = null
+
+        fun getInstance(context: Context) : GhatGPTDatabase {
+            synchronized(this){
+                return INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    GhatGPTDatabase::class.java,
+                    "chat_gpt_db"
+                ).build()
+                    .also {
+                        INSTANCE = it
+                    }
+            }
+        }
+
+    }
 }
