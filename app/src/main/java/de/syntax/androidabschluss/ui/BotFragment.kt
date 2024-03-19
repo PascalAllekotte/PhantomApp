703c3e8ace586dbcf2cd2f5565f87c05d5c1108b
pascalallekotte <pascalallekotte1@web.de>
2024-03-19 19:04:26 +0100

    botfragment

--- app/src/main/java/de/syntax/androidabschluss/ui/BotFragment.kt
+++ app/src/main/java/de/syntax/androidabschluss/ui/BotFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputLayout
 import de.syntax.androidabschluss.R
 import de.syntax.androidabschluss.databinding.FragmentBotBinding
 
@@ -34,6 +38,34 @@ class BotFragment : Fragment() {
         binding.btnGpt.setOnClickListener {
             findNavController().navigate(R.id.action_botFragment_to_gptFragment)
         }
+    }
+
+    private fun addAssistant(view: View){
+        val edAssistantName = EditText(view.context)
+        edAssistantName.hint = "Assistantname eingeben:"
+        edAssistantName.maxLines = 3
+
+        val textInputLayout = TextInputLayout(view.context)
+        val container = FrameLayout(view.context)
+        val params = FrameLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+
+        params.setMargins(50, 30, 50, 30)
+        textInputLayout.layoutParams = params
+
+        textInputLayout.addView(edAssistantName)
+        container.addView(textInputLayout)
+
+        MaterialAlertDialogBuilder(view.context)
+            .setTitle("Add a new robot")
+            .setView(container)
+            .setCancelable(false)
+            .setPositiveButton("Add"){
+
+            }
+
     }
 
 }
