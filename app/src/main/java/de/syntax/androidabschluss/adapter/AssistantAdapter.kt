e69a95a8bdd1f0ffb57424b97ca801d287c7c325
pascalallekotte <pascalallekotte1@web.de>
2024-03-20 23:48:37 +0100

    botfragment

--- app/src/main/java/de/syntax/androidabschluss/adapter/AssistantAdapter.kt
+++ app/src/main/java/de/syntax/androidabschluss/adapter/AssistantAdapter.kt
@@ -1,5 +1,6 @@
 package de.syntax.androidabschluss.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -7,7 +8,10 @@ import androidx.recyclerview.widget.RecyclerView
 import de.syntax.androidabschluss.data.model.open.Assistant
 import de.syntax.androidabschluss.databinding.BotviewLayoutBinding
 
-class AssistantAdapter : ListAdapter<Assistant, AssistantAdapter.ViewHolder>(DiffCallback()) {
+class AssistantAdapter(
+    private val onClickdeleteUpdateCallback : (type: String, position: Int, assistant: Assistant) -> Unit
+
+) : ListAdapter<Assistant, AssistantAdapter.ViewHolder>(DiffCallback()) {
 
     class DiffCallback : DiffUtil.ItemCallback<Assistant>() {
         override fun areItemsTheSame(oldItem: Assistant, newItem: Assistant): Boolean {
@@ -19,16 +23,39 @@ class AssistantAdapter : ListAdapter<Assistant, AssistantAdapter.ViewHolder>(Dif
         }
     }
 
-    class ViewHolder(private val botviewLayoutBinding: BotviewLayoutBinding)
+    class ViewHolder(val botviewLayoutBinding: BotviewLayoutBinding)
         : RecyclerView.ViewHolder(botviewLayoutBinding.root) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        return ViewHolder(
+            BotviewLayoutBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val assistant = getItem(position)
+        holder.botviewLayoutBinding.botTitle.text = assistant.assistantName
+        holder.botviewLayoutBinding.botImage.setImageResource(assistant.assistantImg)
+
+        holder.itemView.setOnClickListener {
+            if (holder.adapterPosition != -1) {
+                onClickdeleteUpdateCallback("click", holder.adapterPosition, assistant)            }
+        }
+        holder.botviewLayoutBinding.editview.setOnClickListener {
+            if (holder.adapterPosition != -1) {
+                onClickdeleteUpdateCallback("delete", holder.adapterPosition, assistant)            }
+        }
+        holder.botviewLayoutBinding.deleteview.setOnClickListener {
+            if (holder.adapterPosition != -1) {
+                onClickdeleteUpdateCallback("update", holder.adapterPosition, assistant)            }
+        }
+
     }
+
 }
