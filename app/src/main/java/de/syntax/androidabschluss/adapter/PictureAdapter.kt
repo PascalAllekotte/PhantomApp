d907510a4a05fd5fbaedda4ba75dfc2e9194daf4
pascalallekotte <pascalallekotte1@web.de>
2024-04-02 13:32:26 +0200

    onlongclick auf bild einmgefügt

--- app/src/main/java/de/syntax/androidabschluss/adapter/PictureAdapter.kt
+++ app/src/main/java/de/syntax/androidabschluss/adapter/PictureAdapter.kt
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import de.syntax.androidabschluss.R
-import de.syntax.androidabschluss.data.model.open.PictureItem // Angenommen, dies ist Ihre Entity-Klasse
+import de.syntax.androidabschluss.data.model.open.PictureItem
 import de.syntax.androidabschluss.databinding.ViewImageLayoutBinding
 
 class PictureAdapter(
@@ -15,12 +15,15 @@ class PictureAdapter(
     private val onLongClickCallback: (position: Int, pictureItem: PictureItem) -> Unit
 ) : ListAdapter<PictureItem, PictureAdapter.ViewHolder>(DiffCallback()) {
 
+    // Mit DiffCallback erkennen wir Änderungen in der Liste effizient
     class DiffCallback : DiffUtil.ItemCallback<PictureItem>() {
         override fun areItemsTheSame(oldItem: PictureItem, newItem: PictureItem): Boolean {
+            // Vergleicht, ob zwei Bildobjekte dieselbe ID haben
             return oldItem.id == newItem.id
         }
 
         override fun areContentsTheSame(oldItem: PictureItem, newItem: PictureItem): Boolean {
+            // Überprüft, ob der Inhalt von zwei Bildobjekten identisch ist
             return oldItem == newItem
         }
     }
@@ -28,20 +31,22 @@ class PictureAdapter(
     inner class ViewHolder(val binding: ViewImageLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(pictureItem: PictureItem, onClick: (position: Int, pictureItem: PictureItem) -> Unit,
                  onLongClick: (position: Int, pictureItem: PictureItem) -> Unit) {
+            // Hier nutzen wir Glide, um das Bild zu laden und anzuzeigen
             Glide.with(binding.loadImage2)
                 .load(pictureItem.url)
-                .placeholder(R.drawable.ic_placeholder)
+                .placeholder(R.drawable.ic_placeholder) // Zeigt ein Standardbild, während das eigentliche Bild lädt
                 .into(binding.loadImage2)
 
-
+            // Click-Listener, um auf Klicks auf das Bild zu reagieren
             itemView.setOnClickListener {
                 onClick(adapterPosition, pictureItem)
             }
 
+            // LongClick-Listener, um auf lange Klicks zu reagieren
             itemView.setOnLongClickListener {
                 onLongClick(adapterPosition, pictureItem)
-                true
-                }
+                true // Gibt true zurück, da das item hier behandelt wird
+            }
         }
     }
 
@@ -54,8 +59,11 @@ class PictureAdapter(
         return ViewHolder(binding)
     }
 
+    // Verknüpft ein Bild-Item mit einem ViewHolder
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // Holt das aktuelle Bild-Item basierend auf der Position
         val pictureItem = getItem(position)
+        // Bindet das Bild-Item und seine Daten an den ViewHolder
         holder.bind(pictureItem, onClickCallback, onLongClickCallback)
     }
 }
