78d594ec378e5a1f9a9f90821682e69fc3255c5e
pascalallekotte <pascalallekotte1@web.de>
2024-04-22 18:19:06 +0200

    graph chart mit erkennung beim klicken der wetteranzeige

--- app/src/main/java/de/syntax/androidabschluss/ui/WeatherFragment.kt
+++ app/src/main/java/de/syntax/androidabschluss/ui/WeatherFragment.kt
@@ -1,5 +1,6 @@
 package de.syntax.androidabschluss.ui
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import de.syntax.androidabschluss.adapter.ForeCastAdapter
 import de.syntax.androidabschluss.data.model.open.CurrentResponseApi
 import de.syntax.androidabschluss.data.model.open.ForecastResponseApi
@@ -18,10 +25,15 @@ import de.syntax.androidabschluss.viewmodel.WeatherViewModel
 import eightbitlab.com.blurview.RenderScriptBlur
 import retrofit2.Call
 import retrofit2.Response
+import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 
-class WeatherFragment : Fragment() {
+
+
+class WeatherFragment : Fragment(), OnChartValueSelectedListener {
 
     private lateinit var binding : FragmentWeatherBinding
     private val weatherViewModel: WeatherViewModel by viewModels()
@@ -36,9 +48,11 @@ class WeatherFragment : Fragment() {
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.chart.setOnChartValueSelectedListener(this)
 
         binding.toolbarLayout2.backbutton.setOnClickListener {
             findNavController().popBackStack()
@@ -90,6 +104,7 @@ class WeatherFragment : Fragment() {
 
             })
 
+
             //Setting Blue View
             var radius=10f
             val decorView = requireActivity().window.decorView
@@ -125,6 +140,7 @@ class WeatherFragment : Fragment() {
                             )
                             adapter = forecastAdapter
                         }
+                        setChartData()
                     }
                 }
             }
@@ -139,9 +155,75 @@ class WeatherFragment : Fragment() {
         }
 
     }
+    override fun onValueSelected(e: Entry?, h: Highlight?) {
+        // Formatieren und Anzeigen der Daten im Textfeld
+        e?.let {
+            val index = it.x.toInt()
+            if (index >= 0 && index < forecastAdapter.differ.currentList.size) {
+                val temperature = forecastAdapter.differ.currentList[index].main?.temp ?: "-"
+                val dateMillis = forecastAdapter.differ.currentList[index].dt?.times(1000L) ?: 0L
+                val date =
+                    SimpleDateFormat("EEEE, d MMM yyyy", Locale.GERMANY).format(Date(dateMillis))
+                val displayText = "Temperatur: $temperature°C am $date"
+                binding.detailTextView.text =
+                    displayText  // Angenommen, `detailTextView` ist Ihr Textfeld
+            }
+        }
+    }
+        override fun onNothingSelected() {
+            binding.detailTextView.text = ""  // Löscht das Textfeld, wenn keine Selektion vorliegt
+        }
+
+    private fun setChartData() {
+        val entries = ArrayList<Entry>()  // Liste für die Datenpunkte
+        val dateValues = ArrayList<Long>() // Liste für die UNIX-Zeitstempel der Datenpunkte
+
+        for (i in forecastAdapter.differ.currentList.indices) {
+            val temperature = forecastAdapter.differ.currentList[i].main?.temp?.toFloat() ?: 0f
+            val dateMillis =
+                forecastAdapter.differ.currentList[i].dt?.times(1000L)  // angenommen 'dt' ist der UNIX-Zeitstempel in Sekunden
+            entries.add(Entry(i.toFloat(), temperature))
+            if (dateMillis != null) {
+                dateValues.add(dateMillis)
+            }
+        }
+
+        val dataSet = LineDataSet(entries, "Temperatur")
+        dataSet.fillAlpha = 110
+        dataSet.color = Color.MAGENTA
+        dataSet.valueTextColor = Color.YELLOW
+        dataSet.valueTextSize = 12f
+        binding.chart.xAxis.textColor = Color.WHITE
+        binding.chart.axisLeft.textColor = Color.WHITE
+        binding.chart.axisRight.textColor = Color.WHITE
+        binding.chart.legend.textColor = Color.WHITE
+
+
+        val lineData = LineData(dataSet)
+        binding.chart.data = lineData
+
+        // Setzen des ValueFormatters für die X-Achse
+        binding.chart.xAxis.valueFormatter = DayAxisValueFormatter(dateValues)
+        binding.chart.xAxis.granularity = 1f  // Stellt sicher, dass jeder Index ein Label erhält
+
+        binding.chart.invalidate() // Aktualisiert das Diagramm
+    }
 
 
 
 
+}
 
+
+class DayAxisValueFormatter(private val dates: List<Long>) : ValueFormatter() {
+    private val dateFormatter = SimpleDateFormat("EE", Locale.GERMANY)
+
+    override fun getFormattedValue(value: Float): String {
+        // value ist der Index des Datenpunkts, der in eine Position in der Liste `dates` umgewandelt wird.
+        return if (value.toInt() >= 0 && value.toInt() < dates.size) {
+            dateFormatter.format(dates[value.toInt()])
+        } else {
+            ""
+        }
+    }
 }
