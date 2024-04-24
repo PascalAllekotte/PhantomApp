3533bff5cca3778d5be44a949c5efc5027d1e362
pascalallekotte <pascalallekotte1@web.de>
2024-04-24 13:29:40 +0200

    kommentare

--- app/src/main/java/de/syntax/androidabschluss/adapter/ChatAdapter.kt
+++ app/src/main/java/de/syntax/androidabschluss/adapter/ChatAdapter.kt
@@ -20,19 +20,18 @@ ListAdapter<Chat, RecyclerView.ViewHolder>(DiffCallback()){
 class SenderViewHolder(private val senderItemBinding: SenderItemBinding):
     RecyclerView.ViewHolder(senderItemBinding.root){
 
+        // nachrichten binden
         fun bind(chat : Chat){
-
             senderItemBinding.txtMessage.text = chat.message.content
             val dateFormat = SimpleDateFormat("dd-MMM-yyyy hh:mm a", Locale.getDefault())
             senderItemBinding.txtDate.text = dateFormat.format(chat.date)
-
         }
-
     }
+
 
     class ReceiverViewHolder(private val receiverItemBinding: ReceiverItemBinding):
         RecyclerView.ViewHolder(receiverItemBinding.root){
-
+        // nachrichten binden
         fun bind(chat : Chat) {
             if (chat.message.content.isNotEmpty()) {
                 receiverItemBinding.txtMessage.text = chat.message.content
@@ -42,18 +41,17 @@ class SenderViewHolder(private val senderItemBinding: SenderItemBinding):
                 receiverItemBinding.typingLAV.visibility = View.GONE
                 receiverItemBinding.txtDate.visibility = View.VISIBLE
                 receiverItemBinding.txtMessage.visibility = View.VISIBLE
-
             } else {
 
                 receiverItemBinding.typingLAV.visibility = View.VISIBLE
                 receiverItemBinding.txtDate.visibility = View.GONE
                 receiverItemBinding.txtMessage.visibility = View.GONE
-
             }
         }
-
     }
 
+
+    // Erstellt neue ViewHolder empfänger oder sender
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return if (viewType == 1) {
             ReceiverViewHolder(
@@ -61,7 +59,6 @@ class SenderViewHolder(private val senderItemBinding: SenderItemBinding):
                     LayoutInflater.from(parent.context),
                     parent,
                     false
-
                 )
             )
         } else (
@@ -70,15 +67,15 @@ class SenderViewHolder(private val senderItemBinding: SenderItemBinding):
                         LayoutInflater.from(parent.context),
                         parent,
                         false
-
                     )
                 )
         )
     }
 
+
+    // bindet neuen ViewHolder empfänger oder sender
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val chat = getItem(position)
-
         if (chat.message.role == "user"){
             (holder as SenderViewHolder).bind(chat)
         }else{
@@ -94,6 +91,7 @@ class SenderViewHolder(private val senderItemBinding: SenderItemBinding):
     }
 
 
+    // Bestimmt den Typ der View
     override fun getItemViewType(position: Int): Int {
         return if (getItem(position).message.role == "user"){
             0 // senderitem
@@ -103,17 +101,15 @@ class SenderViewHolder(private val senderItemBinding: SenderItemBinding):
     }
 
 
+
+    // Hilfsklasse zur Optimierung von Listenoperationen durch Vergleich der Elemente.
     class DiffCallback : DiffUtil.ItemCallback<Chat>() {
         override fun areItemsTheSame(oldItem: Chat, newItem: Chat): Boolean {
             return oldItem.chatId == newItem.chatId
         }
-
         override fun areContentsTheSame(oldItem: Chat, newItem: Chat): Boolean {
             return oldItem == newItem
 
         }
     }
-
-
-
 }
